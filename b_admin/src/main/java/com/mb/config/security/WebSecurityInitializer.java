5a661a41a3dfb32f70cbc2992d3d367766bd320e
vishalsaini07 <sainivishal.07@gmail.com>
2018-08-18 23:44:58 +0530

    sldkfnsf

--- b_admin/src/main/java/com/mb/config/security/WebSecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.mb.config.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class WebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
--- b_admin/src/main/webapp/WEB-INF/pages/footer.jsp
+++
@@ -1,3 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-  <footer class="footer"> © 2018 All rights reserved. Template designed by MeriBindiya.com</footer>
